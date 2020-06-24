f4236fcf9f90b1eb2f314d7b64886d071d875bce
Aceist000 <46760517+Aceist000@users.noreply.github.com>
2020-06-24 21:02:21 +0400

    Add files via upload

---
+++ Sandro-Chkadua-1/src/test/java/Sandro/Chkadua/SandroChkadua1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Sandro.Chkadua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandroChkadua1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
